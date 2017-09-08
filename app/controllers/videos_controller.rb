33ac8d3ecf8476f736337cbbbeeeda832b2089c3
ynvygy <lugerand2004@yahoo.com>
2017-09-08 10:27:15 +0200

    added update and destroy to videos

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,5 +1,5 @@
 class VideosController < ApplicationController
-	before_action :find_video, only: [:show, :dashboard]
+	before_action :find_video, only: [:show, :edit, :update, :destroy, :dashboard]
 
 	def new
 		@video = @video.new
@@ -21,6 +21,22 @@ class VideosController < ApplicationController
 	def show
 		@videos = Video.find(params[:id])
 		#@videos = Video.where(user_id: @user).order("created_at DESC")
+	end
+
+	def edit 
+	end
+
+	def update
+		if @video.update video_params
+			redirect_to video_path(@video, notice: "Video was succesfully updated!")
+		else 
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@video.destroy
+		redirect_to  root_path
 	end
 
 	def dashboard
