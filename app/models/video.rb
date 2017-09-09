7d9a46d028818cf55b2166363e388e801d5c4916
ynvygy <lugerand2004@yahoo.com>
2017-09-09 22:49:21 +0200

    added video upload with paperclip

--- app/models/video.rb
+++ app/models/video.rb
@@ -2,5 +2,7 @@ class Video < ActiveRecord::Base
   belongs_to :user
 
   has_attached_file :thumbnail, styles: { large: "1000x1000#", medium: "550x550#" }, default_url: "/images/:style/missing.png"
-  validates_attachment_content_type :thumbnail, content_type: /\Aimage\/.*\z/
+  has_attached_file :video, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+
+  validates_attachment_content_type :thumbnail, content_type: [ /\Aimage\/.*\z/ , /\Avideo\/.*\Z/
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170908100511) do
+ActiveRecord::Schema.define(version: 20170909164226) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -45,6 +45,10 @@ ActiveRecord::Schema.define(version: 20170908100511) do
     t.string   "thumbnail_content_type"
     t.integer  "thumbnail_file_size"
     t.datetime "thumbnail_updated_at"
+    t.string   "video_file_name"
+    t.string   "video_content_type"
+    t.integer  "video_file_size"
+    t.datetime "video_updated_at"
   end
 
   add_index "videos", ["user_id"], name: "index_videos_on_user_id"
